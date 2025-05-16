50a4de55a3081acd0088f79ca792f5f82c7898d7
Rendika <rendikarendi96@gmail.com>
2025-05-17 04:39:23 +0700

    coba lagii

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -28,7 +28,8 @@ def main(data_path):
 
     # Set MLflow tracking URI and experiment name
     # mlflow.set_tracking_uri("https://dagshub.com/Rendika7/Eksperimen_SML_Rendika-nurhartanto-suharto.mlflow")
-    # mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")  # Local MLflow server
     
     # Pastikan tidak ada run aktif saat mulai
     if mlflow.active_run():
